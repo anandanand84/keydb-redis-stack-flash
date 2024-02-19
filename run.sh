df06ba640d550e6a1382a2c09ff1b00cca2922c0
Anand Aravindan <aaravindan@dvtrading.co>
2024-02-19 17:23:52 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,3 +8,16 @@ keydb-server /etc/keydb/keydb.conf \
 --storage-provider flash /flash \
 --maxmemory 100M \
 --maxmemory-policy allkeys-lfu
+
+
+# From direct image
+sudo docker rm -f keydb;
+sudo docker volume rm keydbflash;
+sudo docker volume create keydbflash;
+sudo docker run -d -it -p 6379:6379 --name keydb -v $(pwd)/lib:/modules \
+--mount dst=/flash,src=keydbflash \
+eqalpha/keydb keydb-server /etc/keydb/keydb.conf \
+--storage-provider flash /flash --maxmemory 500M --maxmemory-policy allkeys-lfu \
+--loadmodule /modules/rejson.so \
+--loadmodule /modules/redistimeseries.so \
+--loadmodule /modules/redisearch.so
