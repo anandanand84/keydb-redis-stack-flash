79f6130c7f84d532353c589b2f700fc7555c93fe
Anand A <anand.anand84@gmail.com>
2024-02-21 16:21:39 +0000

    Use official images

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
-mkdir -p /keydb/flash;
-chmod a+rw /keydb/flash;
-docker run -d -it -p 6349:6379 \
---name keydb \
--v /keydb/flash:/flash \
-anandanand84/keydb \
-keydb-server /etc/keydb/keydb.conf \
---storage-provider flash /flash \
---maxmemory 100M \
---maxmemory-policy allkeys-lfu
+sudo docker volume create \
+  --driver local \
+  --opt type=none \
+  --opt device=/mnt/docker-volumes/keydb-flash \
+  --opt o=bind \
+  keydbflash
+
+sudo docker rm -f keydb;
+sudo docker volume rm keydbflash;
+sudo docker volume create keydbflash;
+sudo docker run -d -it -p 6379:6379 --name keydb -v $(pwd)/lib:/modules \
+--mount dst=/flash,src=keydbflash \
+eqalpha/keydb:x86_64_v6.3.4 keydb-server /etc/keydb/keydb.conf \
+--storage-provider flash /flash --maxmemory 500M --maxmemory-policy allkeys-lfu \
+--loadmodule /modules/rejson.so \
+--loadmodule /modules/redistimeseries.so \
+--loadmodule /modules/redisearch.so \
+--loadmodule /modules/tickstore.so
+
+
